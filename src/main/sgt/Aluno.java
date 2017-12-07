2cdbab91c2db4f52d6d70740df669c667c895eac
Mendess2526 <pedro.mendes.26@gmail.com>
2017-12-07 16:00:06 +0000

    Adds code generated by Visual Paradigm

--- src/main/sgt/Aluno.java
+++ src/main/sgt/Aluno.java
@@ -1,4 +1,32 @@
 package main.sgt;
 
-public class Aluno extends Utilizador{
+import java.util.Map;
+
+public class Aluno extends Utilizador {
+    private boolean eEspecial;
+    private Map<String, Integer> inscricoes;
+
+    public boolean getEespecial() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setEespecial(boolean eEspecial) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Map<String, Integer> getInscricoes() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void inscrever(String uc, int turno) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void desinscrever(String uc, int turno) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void ativarLogin() {
+        throw new UnsupportedOperationException();
+    }
 }
