4232edfe980cc27a9ca86865d27cea4a9f4e940f
Mendess2526 <pedro.mendes@gmail.com>
2017-12-08 16:54:53 +0000

    Add Sugestao

---
+++ src/main/sgt/Sugestao.java
@@ -0,0 +1,38 @@
+package main.sgt;
+
+public class Sugestao {
+
+	private String uc;
+	private String turno;
+	private String alunoNome;
+	private String alunoNumero;
+
+	/**
+	 * 
+	 * @param uc
+	 * @param turno
+	 * @param alunoNome
+	 * @param alunoNumero
+	 */
+	Sugestao(String uc, int turno, String alunoNome, String alunoNumero) {
+		// TODO - implement Sugestao.Sugestao
+		throw new UnsupportedOperationException();
+	}
+
+	String getUc() {
+		return this.uc;
+	}
+
+	String getTurno() {
+		return this.turno;
+	}
+
+	String getAlunoNome() {
+		return this.alunoNome;
+	}
+
+	String getAlunoNumero() {
+		return this.alunoNumero;
+	}
+
+}
