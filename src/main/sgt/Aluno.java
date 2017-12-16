d982fa08ebb46f7e18f49d0dee692024911ca7ff
Mendess2526 <pedro.mendes@gmail.com>
2017-12-16 17:15:54 +0000

    Implements Aluno#ativarLogin

--- src/main/sgt/Aluno.java
+++ src/main/sgt/Aluno.java
@@ -1,12 +1,19 @@
 package main.sgt;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 class Aluno extends Utilizador {
 
 	private boolean eEspecial;
-	private Map<String, Integer> horario;
+    private Map<String, Integer> horario;
 
 	/**
 	 * O construtor do aluno
@@ -17,7 +24,7 @@ class Aluno extends Utilizador {
 	 * @param eEspecial <tt>true</tt> se o aluno tiver estatuto especial
 	 * @param inscricoes As ucs e turnos a que o aluno esta inscrito
 	 */
-	Aluno(String userNum, String password, String email, String name, boolean eEspecial, Map<String, Integer> inscricoes) {
+    Aluno(String userNum, String password, String email, String name, boolean eEspecial, Map<String, Integer> inscricoes) {
 		super(userNum,password,email,name);
 		this.eEspecial = eEspecial;
 		this.horario = inscricoes;
@@ -69,8 +76,40 @@ class Aluno extends Utilizador {
      * Ativa o login deste aluno, enviando lhe um email com o seu numero e a sua password.
      */
 	void ativarLogin() {
-		//TODO - implement Aluno.ativarLogin
-		throw new UnsupportedOperationException();
+		int tries = 0;
+		boolean success = false;
+        while (!success && tries != 5) {
+            success = sendEmail(this.getEmail());
+            tries++;
+        }
 	}
-
+    private boolean sendEmail(String to){
+        String host = "smtp.gmail.com";
+        Properties props = System.getProperties();
+        props.put("mail.smtp.starttls.enable","true");
+        props.put("mail.smtp.host",host);
+        props.put("mail.smtp.user", "swap.dss.uminho@gmail.com");
+        props.put("mail.smtp.password", "swapdssuminho");
+        props.put("mail.smtp.prot",587);
+        props.put("mail.smtp.auth","true");
+        Session session = Session.getDefaultInstance(props,null);
+        MimeMessage mimeMessage = new MimeMessage(session);
+        try {
+            mimeMessage.setFrom(new InternetAddress("swap.dss.uminho@gmail.com"));
+            mimeMessage.setRecipient(Message.RecipientType.TO,new InternetAddress(to));
+            mimeMessage.setSubject("Your swap account is ready");
+            mimeMessage.setText("Your swap account is active.\n\n"
+                                + "Here are your credentials:\n"
+                                + "User Number: "+  super.getUserNum() +"\n"
+                                + "User password: "+super.getPassword()+"\n");
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, "swap.dss.uminho@gmail.com", "swapdssuminho");
+            transport.sendMessage(mimeMessage,mimeMessage.getAllRecipients());
+            transport.close();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
