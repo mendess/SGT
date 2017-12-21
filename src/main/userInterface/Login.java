ae6658dceaa900e0e00a53e870387b544b98e569
Mendess2526 <pedro.mendes.26@gmail.com>
2017-12-21 15:49:32 +0000

    Refactors. Adds Admin

--- src/main/userInterface/Login.java
+++ src/main/userInterface/Login.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package userInterface;
+package main.userInterface;
 
 import javax.swing.JOptionPane;
 import main.sgt.exceptions.WrongCredentialsException;
